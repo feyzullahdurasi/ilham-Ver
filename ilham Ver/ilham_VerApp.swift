4fc2f03782c65e36557429852a5f34efd94c61f7
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-06-18 19:33:23 +0300

    Initial Commit

---
+++ ilham Ver/ilham_VerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ilham_VerApp.swift
+//  ilham Ver
+//
+//  Created by Feyzullah Durası on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ilham_VerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
